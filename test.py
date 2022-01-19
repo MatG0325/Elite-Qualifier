bad9bfc3218c9e150cc7c11758b57d58ee656e17
Mateo Garcia <97066677+MatG0325@users.noreply.github.com>
2022-01-19 23:12:42 +0000

    Added test.py file

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+# from main import
+"""
+class TestIsOdd(unittest.TestCase):
+
+    def test_odd_num(self):
+      self.assertTrue()
+
+      
+
+if __name__ == '__main__':
+    unittest.main()
+"""
