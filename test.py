ce332aab9585982bc3fed0013ace6977c656f403
Mateo Garcia <97066677+MatG0325@users.noreply.github.com>
2022-01-19 23:10:26 +0000

    Several changes, undocumented

---
+++ test.py
@@ -0,0 +1,16 @@
+# This is where tests should be run to determine functionality of
+# code. 
+
+import unittest
+# from main import 
+"""
+class Test(unittest.TestCase):
+
+    def test_response(self):
+        self.assertTrue()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+"""
